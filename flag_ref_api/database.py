6ef5a845d413eaf65b37e3afaaf3aec6cd26258f
Arpan Pandey <arpanpandey.aps@gmail.com>
2023-09-13 07:53:04 +0530

    Fix>

--- flag_ref_api/database.py
+++ flag_ref_api/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+
 load_dotenv()
 
 import os
@@ -9,9 +10,8 @@ import os
 SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
